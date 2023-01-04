ebc1c116c31a569b2fcc1af64cafaac9e74b0c45
isramadani <113502867+isramadani@users.noreply.github.com>
2023-01-04 20:05:51 +0700

    Add files via upload

---
+++ src/test/java/com/israa/mahasiswa/service/MahasiswaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israa.mahasiswa.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahasiswaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
